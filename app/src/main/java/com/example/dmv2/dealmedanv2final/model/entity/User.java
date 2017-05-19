999cff57d19210c50ef3e35582af22274df73de1
King Barat <rinaldihtb@ymail.com>
2017-05-20 06:43:45 +0700

    Generate MPV #1

---
+++ app/src/main/java/com/example/dmv2/dealmedanv2final/model/entity/User.java
@@ -0,0 +1,8 @@
+package com.example.dmv2.dealmedanv2final.model.entity;
+
+/**
+ * Created by CM on 5/20/2017.
+ */
+
+public class User {
+}
---
+++ app/src/main/java/com/example/dmv2/dealmedanv2final/model/session/SessionManager.java
@@ -0,0 +1,8 @@
+package com.example.dmv2.dealmedanv2final.model.session;
+
+/**
+ * Created by CM on 5/20/2017.
+ */
+
+public class SessionManager {
+}
